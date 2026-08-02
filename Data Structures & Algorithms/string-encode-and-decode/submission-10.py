2f3a9975b978bc85088f9dc33db953c4d8356af1
$hakib <50301912+shr-2025@users.noreply.github.com>
2026-08-02 15:23:54 +0200

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        result = ""
+        for el in strs:
+            result += f"{len(el)} {el}"
+        return result
+
+    def decode(self, s: str) -> List[str]:
+        inx = 0
+        result = []
+        n = len(s)
+        while inx < n:
+            count_str = ""
+            while s[inx] != " ":
+                count_str += s[inx]
+                inx += 1
+
+            count = int(count_str)
+            # Move index past the delimiter
+            inx += 1
+            
+            next_str = s[inx : inx + count]
+            inx += count
+
+            result.append(next_str)
+
+        return result
