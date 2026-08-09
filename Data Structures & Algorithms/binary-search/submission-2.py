61b122ba70372a09c3a91769fda1d1da51cdb441
$hakib <50301912+shr-2025@users.noreply.github.com>
2026-08-09 12:08:24 +0200

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def perform_search(down: int, up: int, nums: List[int], target: int):
+            mid = (down + up) // 2
+            if target == nums[mid]:
+                return mid
+            if target == nums[up]:
+                return up
+            if target == nums[down]:
+                return down
+            if mid == down:
+                return -1
+            if target > nums[mid]:
+                return perform_search(down=mid + 1, up=up, nums=nums, target=target)
+            else:
+                return perform_search(down=down, up=mid - 1, nums=nums, target=target)
+        
+        down = 0 
+        up = len(nums) - 1
+
+        return perform_search(down=down, up=up, nums=nums, target=target) 
+        
